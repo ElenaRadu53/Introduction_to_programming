cb1b10aa5f108d0dcc0988d12ed77296d8fe7a8b
ElenaRadu53 <radulouise87@gmail.com>
2024-11-08 14:12:33 +0000

    Assessment 3

---
+++ grades.py
@@ -0,0 +1,16 @@
+grade = int(input("Enter your grade"))
+if grade >= 70:
+    print("Grade A")
+elif grade >= 60 :
+    print("Grade B")
+elif grade >= 50:
+    print("Grade C")
+elif grade >= 40:
+    print("Grade D")
+elif grade >= 30:
+    print("Grade E")
+elif grade>= 20:
+    print("Grade F")
+else:
+    print("Invalid")
+    
